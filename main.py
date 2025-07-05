31a8fd2b62056d62b8fc9635791e6649954ecf46
smithwithatypo <smithwithatypo@gmail.com>
2025-07-05 10:26:51 -0500

    add system prompt templates and output formatted tempaltes

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import os
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+from system_templates import system_prompts
+
 
 # config
 load_dotenv()
 os.environ["ANTHROPIC_API_KEY"] = os.getenv("ANTHROPIC_API_KEY")
-
 model = init_chat_model("claude-sonnet-4-20250514", model_provider="anthropic")
 
 
-# messages
-messages = [
-    SystemMessage("Translate the following from English into Italian"),
-    HumanMessage("hi!"),
-]
+# prompt
+system_template = system_prompts.get("translate")
 
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), 
+     ("user", "{text}")]
+)
+
+prompt = prompt_template.invoke({"language": "French", "text": "hi!"})
 
 # output
-print(model.invoke(messages))
-for token in model.stream(messages):
-    print(token.content, end=" | ")
+# print(prompt)
+# print(prompt.to_messages())
+
+response = model.invoke(prompt)
+print(response.content)
---
+++ system_templates.py
@@ -0,0 +1,4 @@
+system_prompts = dict()   # {title: prompt}
+
+system_prompts["default"] = "Be a helpful assistant"
+system_prompts["translate"] = "Translate the following from English into {language}"
