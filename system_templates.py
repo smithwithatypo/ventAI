ed15da145e56738996c1d1b1bd5900089f48f63d
smithwithatypo <smithwithatypo@gmail.com>
2025-07-05 12:42:12 -0500

    refactor file structure and add personal data file

--- system_templates.py
+++
@@ -1,4 +0,0 @@
-system_prompts = dict()   # {title: prompt}
-
-system_prompts["default"] = "Be a helpful assistant"
-system_prompts["translate"] = "Translate the following from English into {language}"
